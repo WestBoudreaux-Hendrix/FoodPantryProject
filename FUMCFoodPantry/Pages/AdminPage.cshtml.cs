319d70e88181c6d7cc78f882c5f01c8573265b53
SamCT <colliertenisonsa@hendrix.edu>
2026-04-17 13:01:30 -0500

    Very Basic Admin Page

---
+++ FUMCFoodPantry/Pages/AdminPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FUMCFoodPantry.Pages;
+
+public class AdminPage : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
