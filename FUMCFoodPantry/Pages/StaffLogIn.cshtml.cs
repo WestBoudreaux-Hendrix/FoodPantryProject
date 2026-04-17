06870307dddb4d9f5514204adf0b2de8c4447ec6
kbgrizb <grisbykb@hendrix.edu>
2026-04-17 12:37:53 -0500

    Basic Staff Log-In Page

---
+++ FUMCFoodPantry/Pages/StaffLogIn.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesMovie.Pages;
+
+public class StaffLogInModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
