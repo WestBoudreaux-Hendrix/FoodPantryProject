9b3490ed2319cb8059518400f5b7d6867851bdba
SamCT <colliertenisonsa@hendrix.edu>
2026-04-17 21:29:37 -0500

    Basic Order Page

---
+++ FUMCFoodPantry/Pages/OrderPage.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace FUMCFoodPantry.Pages;
+
+public class Order : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
