b4262252a6061d124612bdce30d4fba00f4eb266
JeffreyKamangaHDX <kamangajr@hendrix.edu>
2026-04-19 00:50:07 -0500

    Making restrictions for Stock model

--- FUMCFoodPantry/Models/Stock.cs
+++ FUMCFoodPantry/Models/Stock.cs
@@ -3,10 +3,16 @@ using System.ComponentModel.DataAnnotations;
 public class Stock
 {
     public int Id { get; set; }
+
+    [Required]
     public string Item { get; set; }
+
+    [Range(0,int.MaxValue)]
     public int Quantity { get; set; }
     public double AverageRating { get; set; }
     public int Served { get; set; }
     public DateTime Date { get; set; }
+
+    [Range(0,5)]
     public int Rating { get; set;}
 }
