f04f290f989ddbc6bda3d2f258d1ef3fb85ea342
westboudreaux <boudreauxwh@hendrix.edu>
2026-04-27 12:29:05 -0500

    new user page created

---
+++ FUMCFoodPantry/Pages/NewUserPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FUMCFoodPantry.Pages;
+
+public class NewUserPageModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
