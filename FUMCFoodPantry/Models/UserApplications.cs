4341ab2058c6b6e6fae711c686779e11f14a8ebb
JeffreyKamangaHDX <kamangajr@hendrix.edu>
2026-04-27 12:51:49 -0500

    String to string, un required fields

--- FUMCFoodPantry/Models/UserApplications.cs
+++ FUMCFoodPantry/Models/UserApplications.cs
@@ -2,26 +2,22 @@ using System.ComponentModel.DataAnnotations;
 
 public class UserApplications
 {
-    [Required]
     public int Id { get; set; }
-    [Required]
     public int MemberId { get; set;}
-    [Required]
     public DateTime Date { get; set;}
     [Required]
-    public String FirstName { get; set;}
+    public string FirstName { get; set;}
+    public string MiddleName { get; set;}
     [Required]
-    public String MiddleName { get; set;}
+    public string LastName { get; set;}
     [Required]
-    public String LastName { get; set;}
+    public string Address { get; set;}
     [Required]
-    public String Address { get; set;}
-    [Required]
-    public String City { get; set;}
+    public string City { get; set;}
     [Required]
     [StringLength(2, MinimumLength = 2)]
     [RegularExpression("^[A-Z]{2}$", ErrorMessage = "State must be a valid 2-letter abbreviation.")]
-    public String State { get; set;}
+    public string State { get; set;}
     [Required]
     public int Zip { get; set;}
 
