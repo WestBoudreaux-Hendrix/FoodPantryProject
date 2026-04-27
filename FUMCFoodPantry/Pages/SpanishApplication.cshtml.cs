1946a46adce8dbcded720d799c7da55ca845712d
westboudreaux <boudreauxwh@hendrix.edu>
2026-04-27 11:17:36 -0500

    created and started translating application in spanish

---
+++ FUMCFoodPantry/Pages/SpanishApplication.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FUMCFoodPantry.Pages;
+
+public class SpanishApplicationModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
