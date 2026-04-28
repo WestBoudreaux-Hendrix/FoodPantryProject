f5c846b32634edb909179f5f4066e432d4230599
kbgrizb <grisbykb@hendrix.edu>
2026-04-28 17:47:19 -0500

    Added check-in pages

---
+++ FUMCFoodPantry/Pages/CheckIn.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FUMCFoodPantry.Pages;
+
+public class CheckInModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
