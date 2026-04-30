12b68848886a8742c96f4a8ec01cb3b730a523d3
kbgrizb <grisbykb@hendrix.edu>
2026-04-30 18:17:50 -0500

    Minor fixes to application

--- FUMCFoodPantry/Models/UserApplications.cs
+++ FUMCFoodPantry/Models/UserApplications.cs
@@ -9,7 +9,7 @@ public class UserApplications
     public DateTime Date { get; set;}
     [Required]
     public string FirstName { get; set;}
-    public string MiddleName { get; set;}
+    public string? MiddleName { get; set;}
     [Required]
     public string LastName { get; set;}
     [Required]
