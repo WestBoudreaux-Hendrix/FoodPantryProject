86c50b8bdb53d11254b83ad530d5994c701e4cf8
kbgrizb <grisbykb@hendrix.edu>
2026-04-30 21:05:58 -0500

    Started adding resources

--- FUMCFoodPantry/Pages/Resources.cs
+++ FUMCFoodPantry/Pages/Resources.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FUMCFoodPantry.Pages;
 
-public class TestModel : PageModel
+public class ResourcesModel : PageModel
 {
     public void OnGet()
     {
